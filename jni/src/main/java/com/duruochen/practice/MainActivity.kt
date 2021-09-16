9535ec1131a9cda467b30a5011fe5a7219290556
duruochen <duruochen372@163.com>
2021-09-16 18:03:39 +0800

    version = 4

--- jni/src/main/java/com/duruochen/practice/MainActivity.kt
+++ jni/src/main/java/com/duruochen/practice/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    val version = 3
+    val version = 4
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
